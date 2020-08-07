dd862492b3c35e9e6208e992ae9aaaf172905025
iamZlatika <mazurovazv@gmail.com>
2020-08-07 17:31:56 +0300

    install ejs. add render methods witn pages

--- app.js
+++ app.js
@@ -2,25 +2,28 @@ const express = require('express');
 
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
+
+
 app.listen(3000);
 
 
 app.get('/', (req, res) => {
   // res.send('<p>Home Page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
   // res.send('<p>About Page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
-//redirect
-app.get('/about-us', (req, res)=>{
-  res.redirect('/about')
+app.get('/blog/create', (req, res) => {
+  res.render('create')
 })
-
 //404 - must be in the bottom! If there is no match above - use method will run
 app.use((req, res)=> {
-  res.status(404). sendFile('./views/404.html', { root: __dirname })
+  res.status(404).render('404')
 })
